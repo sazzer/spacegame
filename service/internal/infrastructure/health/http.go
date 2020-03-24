b43f125a03fdc3727e0c942f437d4f48dee28cd1
Graham Cox <graham@grahamcox.co.uk>
2020-03-24 13:31:02 +0000

    Actual HTTP handler for the healthchecks

--- service/internal/infrastructure/health/http.go
+++ service/internal/infrastructure/health/http.go
@@ -16,14 +16,42 @@ func NewRoutes(healthchecker Healthchecker) *Routes {
 	return &Routes{healthchecker}
 }
 
-// CheckHealth is the HTTP route for actually checking the health of the system
-func (r *Routes) CheckHealth(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello, World!")
-}
-
 // RegisterRoutes will register some routes with the provided router
 func (r *Routes) RegisterRoutes(router *echo.Echo) error {
 	router.GET("/health", r.CheckHealth)
 
 	return nil
+}
+
+type componentHealthModel struct {
+	Health  Status `json:"health"`
+	Message string `json:"message,omitempty"`
+}
+
+type systemHealthModel struct {
+	Health     Status                          `json:"health"`
+	Components map[string]componentHealthModel `json:"components"`
+}
+
+// CheckHealth is the HTTP route for actually checking the health of the system
+func (r *Routes) CheckHealth(c echo.Context) error {
+	result := r.healthchecker.CheckSystemHealth()
+
+	statusCode := http.StatusOK
+	if result.Health() == HealthFailure {
+		statusCode = http.StatusServiceUnavailable
+	}
+
+	components := map[string]componentHealthModel{}
+	for name, component := range result.Components() {
+		components[name] = componentHealthModel{
+			Health:  component.Health(),
+			Message: component.Message(),
+		}
+	}
+
+	return c.JSON(statusCode, systemHealthModel{
+		Health:     result.Health(),
+		Components: components,
+	})
 }
