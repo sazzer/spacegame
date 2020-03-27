3d8f1194884837e9d36f1f066a280d0974093916
Graham Cox <graham@grahamcox.co.uk>
2020-03-27 07:12:42 +0000

    Load settings from the environment

--- service/src/main.rs
+++ service/src/main.rs
@@ -1,3 +1,26 @@
+use config::{Config, Environment};
+use dotenv::dotenv;
+use serde::Deserialize;
+
+/// The environmental settings that are used for running the app
+#[derive(Debug, Deserialize)]
+struct Settings {
+    pub port: Option<u16>,
+}
+
+impl Settings {
+    /// Construct the settings from the environment
+    fn new() -> Settings {
+        let mut s = Config::new();
+        s.merge(Environment::default()).unwrap();
+
+        s.try_into().unwrap()
+    }
+}
+
+/// Actually start the application
 fn main() {
+    dotenv().ok();
+    let _ = Settings::new();
     spacegame_lib::main()
 }
