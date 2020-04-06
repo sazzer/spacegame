7db52191897ffed0abf8f49d6b20eacf2ba84891
Graham Cox <graham@grahamcox.co.uk>
2020-04-06 13:35:17 +0100

    Load environment variables and set up logging

--- service/cmd/spacegame/main.go
+++ service/cmd/spacegame/main.go
@@ -1,4 +1,14 @@
 package main
 
+import (
+	// Load the environment properties
+	_ "github.com/joho/godotenv/autoload"
+	"github.com/sirupsen/logrus"
+)
+
 func main() {
+	logrus.SetFormatter(&logrus.TextFormatter{
+		FullTimestamp: true,
+	})
+	logrus.WithField("user", "Tobo").Info("logged in")
 }
